3a4dccc26ed567fe18271876aa53317df2847008
Daryna <darinadyadyuk11@gmail.com>
2025-07-24 18:00:30 +0300

    innitial

---
+++ src/components/App/App.tsx
@@ -0,0 +1,18 @@
+import css from "./App.module.css";
+
+function App() {
+
+  return (
+    <>
+      <div className={css.app}>
+        <header className={css.toolbar}>
+          {/* Компонент SearchBox */}
+          {/* Пагінація */}
+          {/* Кнопка створення нотатки */}
+        </header>
+      </div>
+    </>
+  );
+}
+
+export default App;
